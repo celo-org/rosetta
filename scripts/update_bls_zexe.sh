096681550bdcaf07ecceeea1c18d655b84bb4814
Mariano Cortesi <mcortesi@gmail.com>
2020-03-20 18:34:01 -0300

    bls-zexe: removes old version

--- scripts/update_bls_zexe.sh
+++ scripts/update_bls_zexe.sh
@@ -3,6 +3,6 @@
 BRANCH=${1:-master}
 SCRIPTS_DIR=`dirname $0`
 cd $SCRIPTS_DIR/..
-#rm -r ./external/bls-zexe
-#git commit -am"bls-zexe: removes old version"
+rm -r ./external/bls-zexe
+git commit -am"bls-zexe: removes old version"
 git subtree add --prefix external/bls-zexe https://github.com/celo-org/bls-zexe $BRANCH --squash
