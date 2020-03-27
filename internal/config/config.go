9df035bea294aeeff1222ee916fb158fa6a4853c
Mariano Cortesi <mcortesi@gmail.com>
2020-03-27 11:49:10 -0300

    Add Config for celo-blockchain node

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,6 +8,11 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	FornoRC0Url       = "https://rc0-forno.celo-testnet.org/"
+	FornoAlfajoresUrl = "https://alfajores-forno.celo-testnet.org/"
+)
+
 type HttpServerConfig struct {
 	RequestTimeout time.Duration
 	Port           int
@@ -18,13 +23,26 @@ func (hs *HttpServerConfig) ListenAddress() string {
 	return fmt.Sprintf("%s:%d", hs.Interface, hs.Port)
 }
 
+type NodeConfig struct {
+	Uri string
+}
+
 var HttpServer HttpServerConfig
+var Node NodeConfig
 
-func ReadConfig() {
-
+func configureDefaults() {
+	// httpServer
 	viper.SetDefault("httpServer.requestTimeout", "25s")
 	viper.SetDefault("httpServer.port", "8080")
 	viper.SetDefault("httpServer.interface", "")
+
+	// celo node
+	// viper.SetDefault("node.uri", FornoAlfajoresUrl)
+	viper.SetDefault("node.uri", FornoRC0Url)
+}
+
+func ReadConfig() {
+	configureDefaults()
 
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
@@ -39,9 +57,14 @@ func ReadConfig() {
 
 	}
 
+	// httpServer
 	HttpServer.RequestTimeout, err = time.ParseDuration(viper.GetString("httpServer.requestTimeout"))
 	if err != nil {
-		panic(fmt.Errorf("Config: Error reading: %s \n", "httpServer.requestTimeout"))
+		panic(fmt.Errorf("Config: Error reading: %s err: %s\n", "httpServer.requestTimeout", err))
 	}
 	HttpServer.Port = viper.GetInt("httpServer.port")
+
+	// celo node
+	Node.Uri = viper.GetString("node.uri")
+
 }
