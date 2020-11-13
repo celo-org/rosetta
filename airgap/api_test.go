56744be312efcac765cb06809964b5350f70e4c4
Alex Harley <alexbruceharley@gmail.com>
2020-11-13 17:25:04 +0100

    feat: construction endpoints (#111)
    * feat: construction derive endpoint working
    
    * feat: skeletons for remaining construction API endpoints
    
    * chore: one transfer test case working
    
    * chore: function clean up and adding a tests/ folder
    
    * chore: linting
    
    * test: fix reference to logop
    
    * chore: addressing some review comments

--- airgap/api_test.go
+++ airgap/api_test.go
@@ -66,7 +66,7 @@ func TestTransaction(t *testing.T) {
 		gethTx, _ := signedTx.AsGethTransaction()
 
 		v, r, s := gethTx.RawSignatureValues()
-		sig := valuesToSignature(tx.ChainId, v, r, s)
+		sig := ValuesToSignature(tx.ChainId, v, r, s)
 		Ω(sig).Should(Equal(signedTx.Signature))
 
 		addr, err := types.NewEIP155Signer(tx.ChainId).Sender(gethTx)
