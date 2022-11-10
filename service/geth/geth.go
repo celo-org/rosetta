92c7ede1c7fc37523d60fdb7a57c1e144e6f228f
Piers Powlesland <pierspowlesland@gmail.com>
2022-11-10 20:19:25 +0000

    Fix path for static nodes

--- service/geth/geth.go
+++ service/geth/geth.go
@@ -295,7 +295,7 @@ func (gopts GethOpts) IpcFile() string {
 }
 
 func (gopts GethOpts) StaticNodesFile() string {
-	return filepath.Join(gopts.Datadir, "/celo/static-nodes.json")
+	return filepath.Join(gopts.Datadir, "/static-nodes.json")
 }
 
 func chainParamsFromGenesisFile(genesisPath string) *chain.ChainParameters {
