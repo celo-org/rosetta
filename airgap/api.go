2bb848e91e8670116b83ffd4a943e92ddfe211bc
Paul Lange <palango@gmx.de>
2024-04-03 11:25:35 +0200

    Cleanup `airgab.TxMetadata` type
    This commit removes the deprecated fields
    `GatewayFee` and `GatewayFeeRecipient`.
    It also removes the unused field `FeeCurrency`.

--- airgap/api.go
+++ airgap/api.go
@@ -115,17 +115,14 @@ type FilterQueryParams struct {
 }
 
 type TxMetadata struct {
-	From                common.Address
-	Nonce               uint64
-	GasPrice            *big.Int
-	GatewayFeeRecipient *common.Address
-	GatewayFee          *big.Int
-	FeeCurrency         *common.Address
-	To                  common.Address
-	Data                []byte
-	Value               *big.Int
-	Gas                 uint64
-	ChainId             *big.Int
+	From     common.Address
+	Nonce    uint64
+	GasPrice *big.Int
+	To       common.Address
+	Data     []byte
+	Value    *big.Int
+	Gas      uint64
+	ChainId  *big.Int
 }
 
 func (tm *TxMetadata) AsCallMessage() ethereum.CallMsg {
